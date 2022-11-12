72e2d4f5abbb83980eb2482af2f89219af3a2a21
Marcos Januário <mvjanuario@hotmail.com>
2022-11-12 14:00:21 -0300

    Scenario Completed - Move Animations

--- Girlfriend Is Missing/Assets/Scripts/PlayerMovement.cs
+++ Girlfriend Is Missing/Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed = 7f;
     public Rigidbody2D rb;
+    public Animator anim;
     Vector2 movement;
 
     // Update is called once per frame
@@ -18,5 +19,32 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        if (movement.y != 0) {
+            if (movement.y > 0){
+                anim.SetBool("WalkingBack", true);
+            }
+            else {
+                anim.SetBool("WalkingFront", true);
+            }
+        }
+        else {
+            anim.SetBool("WalkingFront", false);
+            anim.SetBool("WalkingBack", false);
+        }
+
+        if (movement.x != 0) {
+            anim.SetBool("WalkingHorizontal", true);
+            if (movement.x > 0) {
+                gameObject.transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else {
+                gameObject.transform.localScale = new Vector3(1, 1, 1);
+            }
+        }
+        else {
+            anim.SetBool("WalkingHorizontal", false);
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
+        }
     }
 }
